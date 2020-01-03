35af4226764091ec4838d90b18aedcb0f59e0910
Donat Przygodzki <donat.przygodzki@generali.com>
2020-01-03 23:47:52 +0100

    26-todoapps-shorthand-notation

---
+++ 26-todoapps-shorthand-notation/src/actions/todo.js
@@ -0,0 +1,12 @@
+const v4 = require('uuid/v4');
+
+export const addTodo = (text) => ({
+    type: 'ADD_TODO',
+    id: v4(),
+    text
+});
+
+export const toggleTodo = (id) => ({
+    type: 'TOGGLE_TODO',
+    id
+});
